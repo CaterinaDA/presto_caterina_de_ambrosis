f6c1d9e73d72c792c557e3abdfde8ebc87546738
Caterina De Ambrosis <caterinadeam@gmail.com>
2025-12-23 16:14:17 +0100

    Correzione delle Routes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PublicController::class, 'homepage'])->name('home');
 
 // Article Controller
-Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create')->middleware('auth');
-Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
-Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show')->middleware('auth');
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::get('/articles/create', [ArticleController::class, 'create'])
+  ->name('articles.create')
+  ->middleware('auth');
+Route::get('/articles/{article}', [ArticleController::class, 'show'])
+  ->name('articles.show')
+  ->middleware('auth');
